a7f794a35308571c819232207487dff27b2f43e5
CharlesOkechukwu <realcharlieok@gmail.com>
2024-02-26 07:33:35 -0800

    get count of objects

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
 """set up rotes for application"""
+from models.city import City
+from models.user import User
+from models.state import State
+from models.review import Review
+from models.amenity import Amenity
+from models.place import Place
 from . import app_views
+from models import storage
+from flask import jsonify
 
 
 @app_views.route("/status")
 def status():
     """return status code of 200 OK"""
     return {"status": "OK"}
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def get_stat():
+    """return the number of objects in each class"""
+    clas = {"users": User, "states": State, "cities": City,
+            "places": Place, "amenities": Amenity, "reviews": Review}
+    stat = {}
+    for key, value in clas.items():
+        stat[key] = storage.count(value)
+    return jsonify(stat)
